32cf53aac16a7004d799184be271afa764102d72
michaelshamrell <michaelshamrell2001@gmail.com>
2022-02-10 09:56:11 -0800

    Turn firstCard and secondCard into items inside the object - cards. Then push the new card into that object array.

--- index.js
+++ index.js
@@ -1,19 +1,43 @@
-// let firstCard = Math.floor(Math.random() * 13) + 1
-// let secondCard = Math.floor(Math.random() * 13) + 1
-let firstCard = 12
-let secondCard = 10
+let firstCard = Math.floor(Math.random() * 13) + 1
+let secondCard = Math.floor(Math.random() * 13) + 1
+let cards = [firstCard, secondCard] // array - ordered list of items
+// let firstCard = 11
+// let secondCard = 10
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-if (sum <= 20) {
-  console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-  console.log("Woohoo! You've got BlackJack 🥳")
-  hasBlackJack = true
-} else {
-  console.log("You're out of the game 😭")
-  isAlive = false
+function startGame() {
+  renderGame()
+}
+
+
+function renderGame(){
+  cardsEl.textContent = "Cards: " + cards[0] +" " + cards[1]
+  sumEl.textContent = "Sum: " + sum
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+  } else if (sum === 21) {
+    message = "Woohoo! You've got BlackJack"
+    hasBlackJack = true
+  } else {
+    message = "You're out of the game"
+    isAlive = false
+  }
+  messageEl.textContent = message
+}
+
+function newCard() {
+  console.log("drawing a new card from the deck")
+  let card = Math.floor(Math.random() * 13) + 1
+  sum += card
+  cards.push(card)
+  console.log(cards)
+  renderGame()
 }
 //CASH OUT!
 //have a variable that reflects whether or no the user got "blackjack"
