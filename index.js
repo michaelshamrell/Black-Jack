1bda6e52926f6de562a7ea1c8d0e5350023d9e46
michaelshamrell <michaelshamrell2001@gmail.com>
2022-02-10 15:18:48 -0800

    create a function that is used for the values of each card variable. Make this function grab a random number between 1 and 13.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = Math.floor(Math.random() * 13) + 1
-let secondCard = Math.floor(Math.random() * 13) + 1
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard] // array - ordered list of items
 // let firstCard = 11
 // let secondCard = 10
@@ -11,36 +11,47 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+function getRandomCard(){
+  return Math.floor(Math.random() * 13 ) + 1
+}
+
 function startGame() {
   renderGame()
 }
 
 
 function renderGame(){
-  cardsEl.textContent = "Cards: " + cards[0] +" " + cards[1]
+  cardsEl.textContent = "Cards: "
   // Create a for loop that renders out all the cards instead of just two
-  sumEl.textContent = "Sum: " + sum
-  if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-  } else if (sum === 21) {
-    message = "Woohoo! You've got BlackJack"
-    hasBlackJack = true
-  } else {
-    message = "You're out of the game"
-    isAlive = false
+  for (let i = 0; i < cards.length; i ++){
+    cardsEl.textContent += cards[i] + " "
   }
-  messageEl.textContent = message
-}
+  
+   //pushing sum to the html
+    sumEl.textContent = "Sum: " + sum
+    //If statement that relays a message back to the user 
+    if (sum <= 20) {
+      message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+      message = "Woohoo! You've got BlackJack"
+      hasBlackJack = true
+    } else {
+      message = "You're out of the game"
+      isAlive = false
+    }
+    //Pushing the message into the html
+    messageEl.textContent = message
+  }
 
+//New cards function
 function newCard() {
   console.log("drawing a new card from the deck")
-  let card = Math.floor(Math.random() * 13) + 1
+  let card = getRandomCard()
   sum += card
   cards.push(card)
   console.log(cards)
   renderGame()
 }
-//CASH OUT!
-//have a variable that reflects whether or no the user got "blackjack"
+
 console.log(hasBlackJack)
 console.log(isAlive)
