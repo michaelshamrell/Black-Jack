dcd60b2e05c2d27f7c9b7ed67b711d6d9319947d
michaelshamrell <michaelshamrell2001@gmail.com>
2022-02-09 18:59:45 -0800

    create if and else statements that define the rules of blackjack. Then add booleans to show whether the user is out of the game or not, using true or false.

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-let firstCard = Math.floor(Math.random() * 13) + 1
-let secondCard = Math.floor(Math.random() * 13) + 1
-
+// let firstCard = Math.floor(Math.random() * 13) + 1
+// let secondCard = Math.floor(Math.random() * 13) + 1
+let firstCard = 12
+let secondCard = 10
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
 
-if (sum < 21) {
+if (sum <= 20) {
   console.log("Do you want to draw a new card? 🙂")
 } else if (sum === 21) {
   console.log("Woohoo! You've got BlackJack 🥳")
+  hasBlackJack = true
 } else {
   console.log("You're out of the game 😭")
+  isAlive = false
 }
+//CASH OUT!
+//have a variable that reflects whether or no the user got "blackjack"
+console.log(hasBlackJack)
+console.log(isAlive)
