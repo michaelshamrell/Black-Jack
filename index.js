1a1b4a3164118bfeadbe1f2fb339aa9599bd7a32
michaelshamrell <michaelshamrell2001@gmail.com>
2022-02-16 12:30:49 -0800

    Create a function inside of the player object, recognize patterns in dot notation between stuff like console.log, player.sayHello(), math.randm. There are dot notations everywhere and the better you understand why they workand how  they work, the more you'll be able to read code effectively.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+//object "Player" that stores name and chips
+let player = {
+  name: "Michael",
+  chips:  200,
+  sayHello: function() {
+    console.log("howdy")
+  }
+}
+
+player.sayHello();
+
 let cards = [] 
 let sum = 0
 let hasBlackJack = false
@@ -6,16 +17,9 @@ let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-
-let player = {
-  name: "Michael",
-  chips:  145
-}
-
-
-
-
 let playerEl = document.getElementById("player-el")
+
+
 playerEl.textContent = player.name + ": $" + player.chips
 
 function getRandomCard(){
