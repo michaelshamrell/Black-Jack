7cbe3d770136376e59f8228ccc9fb6291c900c8a
michaelshamrell <michaelshamrell2001@gmail.com>
2022-02-16 10:39:04 -0800

    Re-assign card and sum variables to be zero until the startgame() function is activated.

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard] // array - ordered list of items
-// let firstCard = 11
-// let secondCard = 10
-let sum = firstCard + secondCard
+let cards = [] 
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+console.log(cards)
 
 function getRandomCard(){
   let randomNumber = Math.floor(Math.random() * 13 ) + 1
@@ -25,6 +22,12 @@ function getRandomCard(){
 }
 
 function startGame() {
+  isAlive = true
+  let firstCard = getRandomCard()
+  let secondCard = getRandomCard()
+  cards = [firstCard, secondCard]
+  sum = firstCard + secondCard
+  isAlive = true
   renderGame()
 }
 
